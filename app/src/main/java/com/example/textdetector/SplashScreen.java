136969150fe2bc63fe790116588234f3523219a0
Keshav Chandra Ray <keshavray1129@gmail.com>
2022-06-27 00:29:10 +0530

    splash screen

---
+++ app/src/main/java/com/example/textdetector/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.textdetector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        },4000);
+
+    }
+}
